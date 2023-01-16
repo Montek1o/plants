ae0683aeca439ab296cc3fb4b5a435d93720a57f
Andrey Nezhdanov <nezhd.an@yandex.ru>
2023-01-16 19:53:07 +0300

    fix: desktop

--- plants/index.js
+++ plants/index.js
@@ -10,7 +10,3 @@ document.querySelectorAll('.accordion__item__button').forEach((el) => {
     }
   })
 })
-
-let check = '1. Верстка валидная +10\n2. Вёрстка семантическая +20\n3. Вёрстка соотвествует макету +48\n4. Требования к css +12\n5. Интерактивность, реализуемая через css +20\nОценка: 110 / 110';
-
-console.log(check);
